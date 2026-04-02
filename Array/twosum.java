8320d255fde73d030c829201946202805b1d8b1e
SATENDRA KUMAR YADAV <princeworks990@gmail.com>
2026-04-02 23:19:42 +0530

    Implement twoSum function in twosum.java

---
+++ Array/twosum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class twosum {
+
+    public static int[] twoSum(int[] nums, int size, int target) {
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int size = 4;
+
+        int[] result = twoSum(nums, size, target);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
